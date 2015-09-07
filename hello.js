7a0c569bb03ac6da4eab455cdc7858d65aff70ab
Kaique da Silva <kaique.developer@gmail.com>
2015-09-07 01:52:13 -0300

    Added underscore to the project Solved players items retrieve content Refactor the method who dispatch request to steam api with bindEnvironment

--- hello.js
+++ hello.js
@@ -1,36 +1,38 @@
 if (Meteor.isClient) {
+  var getPlayerItemsCallback = null;
+  Session.setDefault('playersItems', []);
+
+  getPlayersItemsCallback = Meteor.bindEnvironment(function(err, results) {
+    var res = null;
+    var _results = [];
+
+    if (!err) {
+      res = JSON.parse(results.content).result.items;
+      _results = _.map(res, function (item, i) {
+        return { _id: item.id, _original_id: item.original_id };
+      });
+      Session.set('playersItems', _results);
+    } else {
+      console.log(err);
+    }
+  });
+
+  Meteor
+    .call('getPlayersItems', getPlayersItemsCallback);
 
   Template.hello.helpers({
-    curInv: function () {
-      var res;
-      Meteor.call('s', function(err, results) {
-        if(err) {
-          console.log(err);
-        } else {
-          res = JSON.parse(results.content).result.items;
-          var newResult = [];
-          for(var i = 0; i < res.length; i++) {
-            newResult[i] = {
-              _id          : res[i].id,
-              _original_id : res[i].original_id
-            }
-          }
-          console.log(newResult);
-          return newResult;
-        }
-      });
+    playersItems: function () {
+      return Session.get('playersItems');
     }
   });
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-  });
+  var apiURL = 'http://api.steampowered.com/IEconItems_730/GetPlayerItems/v0001/?key=705778F03D8B8B68BEE5A4EB06109A00&SteamID=76561197986025120';
+
+  Meteor.startup(function () {});
 
   Meteor.methods({
-    s:function() {
-      var result = HTTP.call('GET','http://api.steampowered.com/IEconItems_730/GetPlayerItems/v0001/?key=705778F03D8B8B68BEE5A4EB06109A00&SteamID=76561197986025120');
-      return result;
-    }
-  })
+    getPlayersItems: function () { return HTTP.call('GET', apiURL); }
+  });
 }
